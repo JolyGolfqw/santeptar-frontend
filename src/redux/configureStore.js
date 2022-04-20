bf45a2d2c337f0c409ed760ba14405722eb48513
Rid1 <333rid333@gmail.com>
2022-04-20 10:42:15 +0300

    сделал карточки категорий

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, combineReducers, createStore } from "redux"
 import thunk from "redux-thunk";
+import categories from "./features/categories";
 
 export const store = createStore(combineReducers({
-    
+    categories
 
 }), applyMiddleware(thunk))
