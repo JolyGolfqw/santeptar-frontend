e4666392562e032c4e4c6522bfdfe02848c60f61
Rasul <overlord.ove@mail.ru>
2022-04-20 16:04:53 +0300

    fixiki

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ const App = () => {
       <Books/>
       <ModalBookDesc/>
     </div>
-    
-    </>
   );
 };
 
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,9 @@
 import categories from "./features/categories";
 import application from './features/application'
 import books from "./features/books";
+import { applyMiddleware, combineReducers, createStore } from "redux"
+import thunk from "redux-thunk";
+
 
 
 export const store = createStore(combineReducers({
