2c15715c3e885ca0bf96f79f4fcfa78bb78916c5
sardalov <sarda10v@bk.ru>
2022-04-24 18:29:34 +0300

    404

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -4,17 +4,25 @@ import { loadBooks } from "../../redux/features/books";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "../Books/Books.module.css";
 import ModalBookDesc from "../ModalBookDesc/ModalBookDesc";
+import { useParams } from "react-router-dom";
 
 const Books = () => {
   const books = useSelector((state) => state.books.items);
   const error = useSelector((state) => state.books.error);
   const loading = useSelector((state) => state.books.loading);
   const dispatch = useDispatch();
+  const { id } = useParams();
 
 
   useEffect(() => {
     dispatch(loadBooks());
   }, [dispatch]);
+
+  //!FILTERED-BOOKS-BY-CATEGORY
+  const filteredBooks = books.filter((item) => {
+    if (!id) return true;
+    return item.category === id;
+  });
 
   if (error) {
     return <div className="error">{error}</div>;
@@ -25,7 +33,7 @@ const Books = () => {
 
   return (
     <div className={styles.small_card}>
-      {books.map((item) => {
+      {filteredBooks.map((item) => {
         return <SmallCard item={item} key={item._id} />;
       })}
     </div>
--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -4,13 +4,13 @@ import styles from "./categories.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { loadCategories } from "../../redux/features/categories";
 import { useEffect } from "react";
-
+import { Link } from "react-router-dom";
 const Categories = () => {
   const categories = useSelector((state) => state.categories.items);
   const error = useSelector((state) => state.error);
   const loading = useSelector((state) => state.loading);
   const dispatch = useDispatch();
-  console.log(categories)
+  console.log(categories);
 
   useEffect(() => {
     dispatch(loadCategories());
@@ -26,7 +26,15 @@ const Categories = () => {
   return (
     <div className={styles.category_content}>
       {categories.map((item) => {
-        return <CategoryCard item={item} key={item._id} />
+        return (
+          <Link
+            key={item._id}
+            className={styles.category}
+            to={`/render/category/${item._id}`}
+          >
+            <CategoryCard item={item} />
+          </Link>
+        );
       })}
     </div>
   );
